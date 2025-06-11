30357165ca23c24a8300d2361c7332ef902b8404
Ronald Junior Rivest-Trudel <ronaldjunior.rivesttrudel@hotmail.com>
2025-06-10 23:22:44 -0400

    finish version 1, will need to add more in due time

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const backToTopButton = document.getElementById("back-to-top");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
+    if (window.scrollY > 800) {
       backToTopButton.style.opacity = "1"; // Show when scrolling down
     } else {
       backToTopButton.style.opacity = "0"; // Hide near top
@@ -22,3 +22,23 @@ document.addEventListener("DOMContentLoaded", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
